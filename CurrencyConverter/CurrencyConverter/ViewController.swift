a6b8f950c5e364c57f477fd2f64a9038e04f401b
Rangel Dias <rterraquio@gmail.com>
2021-05-01 12:21:19 -0300

    Project creation

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Rangel Cardoso Dias on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
